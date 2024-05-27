b027eeea8c727a284fe36ed3b777de22379437b8
Rafael Silva de Medeiros <rafaelmdeirosfmm@gmail.com>
2024-05-27 00:00:39 -0400

    feat: Add First Commit

---
+++ gun.gd
@@ -0,0 +1,16 @@
+extends XRToolsPickable
+
+@export var bullet : PackedScene
+
+func action():
+	_shoot_bullet()
+
+func _shoot_bullet():
+	if bullet:
+		var new_scene = bullet.instantiate()
+		print('dasd')
+		if(new_scene):
+			new_scene.top_level = true
+			add_child(new_scene)
+			new_scene.transform = $spawpoint.global_transform
+			new_scene.linear_velocity = new_scene.transform.basis.z * 30.0
